007b0468da45ee668600148a7a80ccf1ed983661
husnafazal <h.ahmed1@alustudent.com>
2023-10-02 23:44:42 +0300

    pycodestyle validation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,6 +3,7 @@
 This module provides a function to transpose a 2D matrix.
 """
 
+
 def matrix_transpose(matrix):
     """Returns the transpose of a 2D matrix."""
     return [list(row) for row in zip(*matrix)]
