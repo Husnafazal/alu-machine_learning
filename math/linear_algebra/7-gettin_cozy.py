a09d79e1202e2c28341e2a235029655ead00f0f6
husnafazal <h.ahmed1@alustudent.com>
2023-10-03 23:18:14 +0300

    commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,10 +8,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1[0]) != len(mat2[0]):
             return None
         return [row.copy() for row in mat1] + [row.copy() for row in mat2]
-    
+
     elif axis == 1:
         if len(mat1) != len(mat2):
             return None
         return [mat1[i].copy() + mat2[i].copy() for i in range(len(mat1))]
-    
+
     return None
