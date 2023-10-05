ca171daeac7cca334a90e929c67333555eb8714a
husnafazal <h.ahmed1@alustudent.com>
2023-10-05 18:35:55 +0300

    commit

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -3,8 +3,6 @@
 Module for transposing a numpy.ndarray.
 """
 
-import numpy as np
-
 def np_transpose(matrix):
     """Returns the transpose of matrix."""
-    return np.transpose(matrix)
+    return matrix.T
