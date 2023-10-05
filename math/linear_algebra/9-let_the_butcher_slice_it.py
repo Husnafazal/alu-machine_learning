fe84c7052c28068c634219de3cbd27b6199ed206
husnafazal <h.ahmed1@alustudent.com>
2023-10-05 18:17:51 +0300

    commit

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module for slicing a matrix."""
 
+
 def slice_matrix(matrix, start_row, end_row):
     """Slices the matrix from start_row to end_row."""
     return matrix[start_row:end_row]
