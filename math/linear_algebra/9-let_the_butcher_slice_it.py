c9759a6b0a765ab4cc6cc922a57c1bed5e8cc6d7
husnafazal <h.ahmed1@alustudent.com>
2023-10-05 18:16:17 +0300

    commit

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python3
-"""
-This module contains a function that multiplies two matrices.
-"""
+"""Module for slicing a matrix."""
 
-
-def mat_mul(mat1, mat2):
-    """Performs matrix multiplication of mat1 and mat2."""
-    # Check columns in mat1 vs rows in mat2
-    if len(mat1[0]) != len(mat2):
-        return None
-
-    # Compute the resulting matrix
-    result = []
-    for i in range(len(mat1)):
-        row_result = []
-        for j in range(len(mat2[0])):
-            # Compute dot product for i-th row of mat1 and j-th column of mat2
-            elements = [mat1[i][k] * mat2[k][j] for k in range(len(mat1[0]))]
-            sum_elements = sum(elements)
-            row_result.append(sum_elements)
-        result.append(row_result)
-
-    return result
+def slice_matrix(matrix, start_row, end_row):
+    """Slices the matrix from start_row to end_row."""
+    return matrix[start_row:end_row]
