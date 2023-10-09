c0b793207962a64461a7b7ba7a00ec0c6048b68b
husnafazal <h.ahmed1@alustudent.com>
2023-10-09 23:45:06 +0300

    commit

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-This module contains functions that perform element-wise operations on matrices.
+Module for element-wise operations on matrices.
 """
 
 
 def np_elementwise(mat1, mat2):
     """
-    Returns a tuple with element-wise addition, subtraction, multiplication, 
-    and division of two matrices.
+    Returns a tuple with element-wise addition, subtraction,
+    multiplication, and division of two matrices.
     """
     addition = mat1 + mat2
     subtraction = mat1 - mat2
