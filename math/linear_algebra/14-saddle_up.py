80bb7a6b803cd17ad124ab0b25dc0f689be82fc1
husnafazal <h.ahmed1@alustudent.com>
2023-10-10 00:01:45 +0300

    commit

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """
+    Perform matrix multiplication on two matrices.
+
+    Parameters:
+    - mat1 (numpy.ndarray): First matrix.
+    - mat2 (numpy.ndarray): Second matrix.
+
+    Returns:
+    - numpy.ndarray: The result of the matrix multiplication.
+    """
+    return np.matmul(mat1, mat2)
