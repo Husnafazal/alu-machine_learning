1cc2403c0f9c5aa659f76f4d608a5b1c0238cd81
husnafazal <h.ahmed1@alustudent.com>
2023-10-18 18:47:33 +0300

    commit

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
 """
-1-correlation module
----------------------
+Multinormal module
+------------------
 
-This module provides a function called "correlation" which calculates
-the correlation matrix for a given covariance matrix. The correlation
-matrix is essential for understanding the linear relationship between
-variables in multivariate data.
-
-Functions:
-- correlation(C): Calculates and returns the correlation matrix of C.
-
+This module provides a class called "MultiNormal" which represents a
+Multivariate Normal distribution.
 """
 
 import numpy as np
 
 
-def correlation(C):
-    """
-    Calculates a correlation matrix.
+class MultiNormal:
+    """Represents a Multivariate Normal distribution"""
     
-    Args:
-    - C: numpy.ndarray -- A covariance matrix.
-    
-    Returns:
-    - R: numpy.ndarray -- The correlation matrix.
-    """
-    
-    if not isinstance(C, np.ndarray):
-        raise TypeError("C must be a numpy.ndarray")
-    
-    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
-        raise ValueError("C must be a 2D square matrix")
-
-    std_devs = np.sqrt(np.diag(C))
-    std_dev_matrix = np.outer(std_devs, std_devs)
-    R = C / std_dev_matrix
-
-    return R
+    def __init__(self, data):
+        """
+        Class constructor
+        
+        Args:
+        - data: numpy.ndarray of shape (d, n) containing the data set
+        
+        Public instance variables:
+        - mean: numpy.ndarray of shape (d, 1) containing the mean of data
+        - cov: numpy.ndarray of shape (d, d) containing the covariance matrix of data
+        """
+        
+        if not isinstance(data, np.ndarray):
+            raise TypeError("data must be a 2D numpy.ndarray")
+        
+        if len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+            
+        d, n = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        
+        # Calculate the mean
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        
+        # Calculate the covariance
+        deviation = data - self.mean
+        self.cov = np.dot(deviation, deviation.T) / (n - 1)
+s
