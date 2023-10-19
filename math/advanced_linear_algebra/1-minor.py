0d9d947cb116b4785847b58989c0c4ca973066e8
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 21:30:43 +0300

    commit

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 
 def determinant(matrix):
-    if len(matrix) == 1:
-        return matrix[0][0]
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    
-    det = 0
-    for j in range(len(matrix)):
-        submatrix = [row[:j] + row[j+1:] for row in matrix[1:]]
-        det += ((-1) ** j) * matrix[0][j] * determinant(submatrix)
-    return det
-
-def minor(matrix):
+    """Calculate the determinant of a matrix"""
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    if not matrix or not matrix[0] or len(matrix) != len(matrix[0]):
-        raise ValueError("matrix must be a non-empty square matrix")
 
     n = len(matrix)
-    minor_matrix = [[0 for _ in range(n)] for _ in range(n)]
+    
+    if n == 0:
+        return 1
+    
+    if not all(len(row) == n for row in matrix):
+        raise ValueError("matrix must be a square matrix")
 
-    for i in range(n):
-        for j in range(n):
-            submatrix = [row[:j] + row[j+1:] for k, row in enumerate(matrix) if k != i]
-            minor_matrix[i][j] = determinant(submatrix)
+    # Base case: 1x1 matrix
+    if n == 1:
+        return matrix[0][0]
 
-    return minor_matrix
+    # Base case: 2x2 matrix
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for j in range(n):
+        # Compute the sub
