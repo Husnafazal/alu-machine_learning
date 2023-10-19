14237ce914fb3c3211513d7c2d53b092fbcfcc36
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 00:37:57 +0300

    commit

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,30 +1,51 @@
-#!/usr/bin/env python3
+def determinant(matrix):
+    """
+    Calculates the determinant of a matrix.
+    """
+    if len(matrix) == 1:
+        return matrix[0][0]
+    elif len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    else:
+        det = 0
+        for i in range(len(matrix)):
+            minor = [row[:i] + row[i+1:] for row in (matrix[:0] + matrix[0+1:])]
+            det += ((-1) ** i) * matrix[0][i] * determinant(minor)
+        return det
+
+def adjugate(matrix):
+    """
+    Calculates the adjugate of a matrix.
+    """
+    adj = []
+    for i in range(len(matrix)):
+        adj_row = []
+        for j in range(len(matrix)):
+            minor = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+            adj_row.append(((-1) ** (i + j)) * determinant(minor))
+        adj.append(adj_row)
+    return list(map(list, zip(*adj)))  # Transpose the matrix
+
 def inverse(matrix):
     """
-    Function that calculates the inverse of a matrix.
-    
-    Arguments:
-     - matrix: list of lists whose inverse should be calculated
-     
-    Returns:
-     - The inverse of matrix, or None if matrix is singular
+    Calculates the inverse of a matrix.
     """
-    
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-
-    n = len(matrix)
     
-    if n == 0 or len(matrix[0]) == 0 or not all(len(row) == n for row in matrix):
+    if not matrix or not all(matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
-    # Calculate determinant
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    if rows != cols:
+        raise ValueError("matrix must be a non-empty square matrix")
+
     det = determinant(matrix)
     if det == 0:
         return None
 
-    # Calculate adjugate
     adj = adjugate(matrix)
 
-    # Compute inverse by dividing each entry of adjugate matrix by determinant
-    return [[adj[row][col] / det for col in range(n)] for row in range(n)]
+    return [[adj[row][col] / det for col in range(cols)] for row in range(rows)]
