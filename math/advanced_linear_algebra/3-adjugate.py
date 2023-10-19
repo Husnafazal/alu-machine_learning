fce9cf4d56eba8b0e73dd9c0a680d54dffb42c72
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 00:15:38 +0300

    commit

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -4,7 +4,6 @@
 Module for calculating the adjugate matrix of a matrix.
 """
 
-
 def minor(matrix):
     """Compute the minor of a matrix."""
     if len(matrix) == 1:
@@ -14,10 +13,10 @@ def minor(matrix):
     for i in range(len(matrix)):
         minors.append([])
         for j in range(len(matrix)):
-            minor_matrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+            rows = matrix[:i] + matrix[i+1:]
+            minor_matrix = [row[:j] + row[j+1:] for row in rows]
             minors[i].append(determinant(minor_matrix))
     return minors
-
 
 def determinant(matrix):
     """Compute the determinant of a matrix."""
@@ -28,23 +27,27 @@ def determinant(matrix):
 
     det = 0
     for j in range(len(matrix)):
-        det += ((-1) ** j) * matrix[0][j] * \
-               determinant([row[:j] + row[j+1:] for row in matrix[1:]])
+        rows = [row[:j] + row[j+1:] for row in matrix[1:]]
+        det += ((-1) ** j) * matrix[0][j] * determinant(rows)
     return det
-
 
 def cofactor(matrix):
     """Compute the cofactor matrix of a matrix."""
     n = len(matrix)
 
-    if n == 0 or len(matrix[0]) == 0 or not all(len(row) == n for row in matrix):
+    if (n == 0 or len(matrix[0]) == 0 
+            or not all(len(row) == n for row in matrix)):
         raise ValueError("matrix must be a non-empty square matrix")
+    
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     minors_mat = minor(matrix)
-    return [[((-1) ** (i + j)) * minors_mat[i][j] for j in range(n)] for i in range(n)]
-
+    cofactors = [
+        [((-1) ** (i + j)) * minors_mat[i][j] for j in range(n)] 
+        for i in range(n)
+    ]
+    return cofactors
 
 def adjugate(matrix):
     """Calculate the adjugate matrix of a matrix."""
