4ae5425b047a2e09318239a9fd18dc926c0198fb
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 20:28:06 +0300

    commit

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Created  the Python script for the determinant function
-cat > 0-determinant.py <<EOF
 def determinant(matrix):
     # Check if matrix is a list of lists
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
@@ -26,38 +24,3 @@ def determinant(matrix):
         submatrix = [row[:j] + row[j+1:] for row in matrix[1:]]
         det += ((-1) ** j) * matrix[0][j] * determinant(submatrix)
     return det
-EOF
-
-# Creating the main Python program to test the determinant function
-cat > 0-main.py <<EOF
-if __name__ == '__main__':
-    determinant = __import__('0-determinant').determinant
-
-    mat0 = [[]]
-    mat1 = [[5]]
-    mat2 = [[1, 2], [3, 4]]
-    mat3 = [[1, 1], [1, 1]]
-    mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-    mat5 = []
-    mat6 = [[1, 2, 3], [4, 5, 6]]
-
-    print(determinant(mat0))  # 1
-    print(determinant(mat1))  # 5
-    print(determinant(mat2))  # -2
-    print(determinant(mat3))  # 0
-    print(determinant(mat4))  # 192
-    try:
-        determinant(mat5)
-    except Exception as e:
-        print(e)  # matrix must be a list of lists
-    try:
-        determinant(mat6)
-    except Exception as e:
-        print(e)  # matrix must be a square matrix
-EOF
-
-# Execute the main Python program
-python3 0-main.py
-
-# Cleanup
-rm 0-determinant.py 0-main.py
