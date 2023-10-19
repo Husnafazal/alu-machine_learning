34b1361d22af78aa7a6b099dcf742152e81bcf55
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 00:51:48 +0300

    commit

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
+"""
+Module to determine the definiteness of a matrix.
+"""
+
 import numpy as np
 
 def definiteness(matrix):
-    """Calculates the definiteness of a matrix."""
+    """
+    Calculates the definiteness of a matrix.
+
+    Args:
+    - matrix (np.ndarray): The matrix whose definiteness should be determined.
+
+    Returns:
+    - str: The definiteness of the matrix. Can be one of the following:
+        * "Positive definite"
+        * "Positive semi-definite"
+        * "Negative definite"
+        * "Negative semi-definite"
+        * "Indefinite"
+    - None: If the matrix is not valid for the definiteness determination.
+    """
     
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
