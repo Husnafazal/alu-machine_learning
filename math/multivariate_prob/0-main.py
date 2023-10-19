5b366262a36cc255b471354d0f790846d2435621
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 16:09:09 +0300

    commit

--- math/multivariate_prob/0-main.py
+++ math/multivariate_prob/0-main.py
@@ -3,9 +3,13 @@
 import numpy as np
 from multinormal import MultiNormal
 
-mn = MultiNormal(...)
-x_list = [[1, 2, 3, 4, 5]]
-x_array = np.array(x_list)
+np.random.seed(0)
+data = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], 10000)
+mean = np.mean(data, axis=0)
+cov = np.cov(data, rowvar=False)
 
-result = mn.pdf(x_array)
-print(result)
+mn = MultiNormal(data)
+
+# Make sure to reshape x into the right format
+x = np.array([[1], [2]])
+print(mn.pdf(x))
--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -6,53 +6,24 @@ class MultiNormal:
     """ Represents a Multivariate Normal distribution """
 
     def __init__(self, data):
-        """Initialize MultiNormal instance
-        
-        Parameters:
-        data (numpy.ndarray): shape (d, n) containing the data set:
-            n is the number of data points
-            d is the number of dimensions in each data point
-        """
-
+        """ Constructor method """
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
-        d, n = data.shape
-
-        self.mean = np.mean(data, axis=1, keepdims=True)
-        deviation = data - self.mean
-        self.cov = np.matmul(deviation, deviation.T) / (n - 1)
-
+        n, d = data.shape
+        self.mean = np.mean(data, axis=0).reshape(1, d)
+        self.cov = np.cov(data.T)
+        
     def pdf(self, x):
-        """Calculate the PDF at a data point
-        
-        Parameters:
-        x (numpy.ndarray): shape (d, 1) containing the data point 
-            whose PDF should be calculated
-            d is the number of dimensions of the MultiNormal instance
-        
-        Returns:
-        float: the value of the PDF
-        """
-
+        """ Calculates the PDF at a data point """
         if not isinstance(x, np.ndarray):
             raise ValueError("x must be a numpy.ndarray")
-
         d = self.cov.shape[0]
-
-        if len(x.shape) != 2 or x.shape[1] != 1:
-            raise ValueError("x must have the shape ({}, 1)".format(d))
-
-        # Mean and covariance
-        mean = self.mean
-        cov = self.cov
-
-        # Determinant and inverse of the covariance matrix
-        det = np.linalg.det(cov)
-        inv = np.linalg.inv(cov)
-
-        # Calculate PDF
-        denominator = np.sqrt(np.power(2 * np.pi, d) * det)
-        mahalanobis = np.dot((x - mean).T, np.dot(inv, (x - mean)))
-
-        return float(np.exp(-0.5 * mahalanobis) / denominator)
+        if len(x.shape) != 2 or x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+        
+        # Calculating the PDF
+        inv = np.linalg.inv(self.cov)
+        det = np.linalg.det(self.cov)
+        expo = np.exp(-0.5 * np.matmul(np.matmul((x - self.mean).T, inv), x - self.mean))
+        return expo / (np.sqrt((2 * np.pi) ** d * det))
