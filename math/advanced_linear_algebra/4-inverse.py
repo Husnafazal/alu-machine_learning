c86c7d2b08439e6b0245e3eefcd5dd3949a03db9
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 01:17:42 +0300

    commit

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
+
 def determinant(matrix):
+    """
+    Calculate the determinant of a matrix.
+
+    Args:
+        matrix (list of list): The matrix for which the determinant is to be calculated.
+
+    Returns:
+        int or float: The determinant of the matrix.
+    """
     # Placeholder for the determinant function
+    return 1  # Placeholder return value
 
 def adjugate(matrix):
+    """
+    Calculate the adjugate of a matrix.
+
+    Args:
+        matrix (list of list): The matrix for which the adjugate is to be calculated.
+
+    Returns:
+        list of list: The adjugate of the matrix.
+    """
     # Placeholder for the adjugate function
+    return matrix  # Placeholder return value
 
 def inverse(matrix):
     """
     Calculate the inverse of a matrix.
-    """
 
+    Args:
+        matrix (list of list): The matrix to be inverted.
+
+    Returns:
+        list of list or None: The inverse of the matrix if it exists, otherwise None.
+    """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
