fd26b0a193e0798371d706e73681f05ad607a814
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 14:22:07 +0300

    commit

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3
-"""
-Multinormal module
-------------------
-
-This module provides a class called "MultiNormal" which represents a
-Multivariate Normal distribution.
-"""
 
 import numpy as np
-
 
 class MultiNormal:
     """Represents a Multivariate Normal distribution"""
     
     def __init__(self, data):
-        """
-        Class constructor
-        
-        Args:
-        - data: numpy.ndarray of shape (d, n) containing the data set
-        
-        Public instance variables:
-        - mean: numpy.ndarray of shape (d, 1) containing the mean of data
-        - cov: numpy.ndarray of shape (d, d) containing the covariance matrix of data
-        """
+        """Initialize MultiNormal"""
         
         if not isinstance(data, np.ndarray):
             raise TypeError("data must be a 2D numpy.ndarray")
@@ -35,9 +18,14 @@ class MultiNormal:
         if n < 2:
             raise ValueError("data must contain multiple data points")
         
-        # Calculate the mean
         self.mean = np.mean(data, axis=1, keepdims=True)
-        
-        # Calculate the covariance
         deviation = data - self.mean
         self.cov = np.dot(deviation, deviation.T) / (n - 1)
+
+    def pdf(self, x):
+        """Basic PDF method"""
+        
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        
+        # Additional computations for the PDF should go here...
