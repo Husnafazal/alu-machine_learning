93400be8d73a72ee8d083c9fac3d34c6ed849b84
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 23:55:50 +0300

    commit

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -32,10 +32,13 @@ def determinant(matrix):
 def cofactor(matrix):
     """Compute the cofactor matrix of a matrix."""
     n = len(matrix)
-    if not all(isinstance(row, list) for row in matrix) or not all(len(row) == n for row in matrix):
+    
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    if n == 0:
+    
+    if n == 0 or len(matrix[0]) == 0 or not all(len(row) == n for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
+    
     minors_mat = minor(matrix)
     return [[((-1) ** (i + j)) * minors_mat[i][j] for j in range(n)] for i in range(n)]
 
