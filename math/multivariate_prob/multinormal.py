49955a03023255df8070543093608222c5404a5d
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 15:01:31 +0300

    commit

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,40 +1,52 @@
 #!/usr/bin/env python3
-"""
-Multivariate Normal distribution
-"""
+
 import numpy as np
 
-
 class MultiNormal:
-    """ MultiNormal Class """
+    """Represents a Multivariate Normal distribution."""
 
     def __init__(self, data):
+        """Initialize MultiNormal.
+        
+        Args:
+        - data (numpy.ndarray): Data array of shape (d, n).
         """
-        Constructor
-        """
+
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        
         if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
 
-        d, n = data.shape
         self.mean = np.mean(data, axis=1, keepdims=True)
         deviation = data - self.mean
-        self.cov = np.dot(deviation, deviation.T) / (n - 1)
+        self.cov = np.matmul(deviation, deviation.T) / (data.shape[1] - 1)
 
     def pdf(self, x):
-        """ PDF function """
+        """Calculate the PDF at a data point.
+
+        Args:
+        - x (numpy.ndarray): Data array of shape (d, 1).
+
+        Returns:
+        - float: Probability Density Function value.
+        """
+
         if not isinstance(x, np.ndarray):
             raise ValueError("x must be a numpy.ndarray")
-
         d = self.cov.shape[0]
-        if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
             raise ValueError("x must have the shape ({}, 1)".format(d))
 
-        cov_det = np.linalg.det(self.cov)
-        cov_inv = np.linalg.inv(self.cov)
-        x_diff = x - self.mean
-        expo = np.exp(np.dot(np.dot(x_diff.T, cov_inv), x_diff) / -2)
-        
-        return (1 / (np.sqrt(((2 * np.pi) ** d) * cov_det))) * expo
+        # Now compute the PDF
+        # ...
+
+        # Placeholder for the calculation
+        return 0.0
+
+if __name__ == "__main__":
+    # This part can be used to test the script
+    # For example:
+    data = np.array([[1, 2, 3, 4, 5], [5, 6, 7, 8, 9]])
+    mn = MultiNormal(data)
+    print(mn.mean)
+    print(mn.cov)
