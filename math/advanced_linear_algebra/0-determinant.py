1e423cce193e3cfbc6ab0b1ee60a48d6ec714c9a
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 21:48:33 +0300

    commit

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -2,7 +2,8 @@
 
 def determinant(matrix):
     """Calculate the determinant of a matrix"""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     n = len(matrix)
