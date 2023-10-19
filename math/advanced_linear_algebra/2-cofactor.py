e9c45c737efb735ae491051582415f6ad9b5c80a
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 23:34:21 +0300

    commit

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -41,7 +41,8 @@ def cofactor(matrix):
     Returns:
     - List of lists. The cofactor matrix.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+    for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     n = len(matrix)
