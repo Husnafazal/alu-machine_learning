d2fe054e2c2ed8f5b3f15298c055d22427719ab2
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 00:11:39 +0300

    commit

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Module for calculating the adjugate matrix of a matrix.
+"""
+
 def minor(matrix):
     """Compute the minor of a matrix."""
     if len(matrix) == 1:
@@ -9,9 +13,10 @@ def minor(matrix):
     for i in range(len(matrix)):
         minors.append([])
         for j in range(len(matrix)):
-            minor_matrix = [row[:j] + row[j+1:] for row in (matrix[:i]+matrix[i+1:])]
+            minor_matrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
             minors[i].append(determinant(minor_matrix))
     return minors
+
 
 def determinant(matrix):
     """Compute the determinant of a matrix."""
@@ -22,26 +27,25 @@ def determinant(matrix):
 
     det = 0
     for j in range(len(matrix)):
-        det += ((-1) ** j) * matrix[0][j] * determinant([row[:j] + row[j+1:] for row in matrix[1:]])
+        det += ((-1) ** j) * matrix[0][j] * \
+               determinant([row[:j] + row[j+1:] for row in matrix[1:]])
     return det
+
 
 def cofactor(matrix):
     """Compute the cofactor matrix of a matrix."""
     n = len(matrix)
 
-    # Check if matrix is square and non-empty
     if n == 0 or len(matrix[0]) == 0 or not all(len(row) == n for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
-
-    # Then, check if it's a list of lists
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     minors_mat = minor(matrix)
     return [[((-1) ** (i + j)) * minors_mat[i][j] for j in range(n)] for i in range(n)]
 
+
 def adjugate(matrix):
     """Calculate the adjugate matrix of a matrix."""
     cofactor_matrix = cofactor(matrix)
-    # Transpose the cofactor matrix to get the adjugate
     return list(map(list, zip(*cofactor_matrix)))
