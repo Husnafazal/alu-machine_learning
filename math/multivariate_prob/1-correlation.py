a5701aca32f4a4c6405505feb588afde5bbd2aff
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 14:35:45 +0300

    commit

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
+"""
+Module for calculating correlation matrix.
+"""
 
 import numpy as np
 
+
 def correlation(C):
     """
-    Calculates a correlation matrix.
+    Calculates the correlation matrix.
+
+    Parameters:
+    - C: numpy.ndarray of shape (d, d) containing a covariance matrix
+
+    Returns:
+    - numpy.ndarray of shape (d, d) containing the correlation matrix
     """
-    # Check if C is a numpy.ndarray
+
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    
-    # Check if C has shape (d, d)
+
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-    
-    # Calculate the standard deviations for each dimension
-    std_devs = np.sqrt(np.diag(C))
-    
-    # Convert the standard deviations to a diagonal matrix for easier calculations
-    std_dev_matrix = np.outer(std_devs, std_devs)
-    
-    # Calculate the correlation matrix
-    R = C / std_dev_matrix
-    
-    return R
+
+    d = C.shape[0]
+    std_dev = np.sqrt(np.diag(C))
+    outer_std_dev = np.outer(std_dev, std_dev)
+    correlation_matrix = C / outer_std_dev
+
+    return correlation_matrix
+
+
+if __name__ == "__main__":
+    C = np.array([[36, -30, 15], [-30, 100, -20], [15, -20, 25]])
+    print(correlation(C))
