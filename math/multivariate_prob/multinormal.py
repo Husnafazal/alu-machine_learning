6c26a10934e7f6e90f597e20b5587140b61d7f37
husnafazal <h.ahmed1@alustudent.com>
2023-10-19 14:29:38 +0300

    xommit

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
+"""
+This module provides the MultiNormal class which represents a Multivariate Normal distribution.
+"""
 
 import numpy as np
 
+
 class MultiNormal:
-    """Represents a Multivariate Normal distribution"""
-    
+    """
+    This class represents a Multivariate Normal distribution.
+    """
+
     def __init__(self, data):
-        """Initialize MultiNormal"""
-        
-        if not isinstance(data, np.ndarray):
+        """
+        Initialize the MultiNormal class.
+
+        Parameters:
+        - data: numpy.ndarray of shape (d, n) containing the data set
+            - n: number of data points
+            - d: number of dimensions in each data point
+        """
+
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        
-        if len(data.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
-            
+
         d, n = data.shape
         if n < 2:
             raise ValueError("data must contain multiple data points")
-        
+
         self.mean = np.mean(data, axis=1, keepdims=True)
         deviation = data - self.mean
-        self.cov = np.dot(deviation, deviation.T) / (n - 1)
-
-    def pdf(self, x):
-        """Basic PDF method"""
-        
-        if not isinstance(x, np.ndarray):
-            raise TypeError("x must be a numpy.ndarray")
-        
-        # Additional computations for the PDF should go here...
+        self.cov = deviation @ deviation.T / (n - 1)
