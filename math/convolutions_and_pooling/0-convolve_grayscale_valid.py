09015eda604abb3ec734841c4b79f88aa0a6dadb
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 19:44:30 +0300

    commit

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
 
+"""
+This module contains a method that performs a valid
+convolution on grayscale images.
+"""
+
 import numpy as np
 
 def convolve_grayscale_valid(images, kernel):
-    # Your convolution implementation goes here
-    pass
+    """
+    Perform a valid convolution on grayscale images.
 
-if __name__ == '__main__':
-    images = np.load('../../supervised_learning/data/MNIST.npz')['X_train']
-    kernel = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
-    images_conv = convolve_grayscale_valid(images, kernel)
+    Args:
+        images (numpy.ndarray): Input images with shape (m, h, w).
+            m: Number of images.
+            h: Height in pixels of the images.
+            w: Width in pixels of the images.
+        kernel (numpy.ndarray): Convolution kernel with shape (kh, kw).
+            kh: Height of the kernel.
+            kw: Width of the kernel.
+
+    Returns:
+        numpy.ndarray: Convolved images with shape (m, output_h, output_w).
+            output_h: Height of the convolved images.
+            output_w: Width of the convolved images.
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+    output = np.zeros((m, output_h, output_w))
+    for i in range(output_h):
+        for j in range(output_w):
+            output[:, i, j] = (kernel * images[:, i: i + kh, j: j + kw])\
+                .sum(axis=(1, 2))
+    return output
