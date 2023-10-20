66df1177cdbf2dccff7cd156bc2660527d4faeb2
husnafazal <h.ahmed1@alustudent.com>
2023-10-20 20:37:45 +0300

    commit

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -22,4 +22,42 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
             kw: Width of the kernels.
             kc: Number of channels in the kernels.
             nc: Number of kernels/filters.
-        padding (str or tuple): Padding mode or tuple of padding values (
+        padding (str or tuple): Padding mode or tuple of padding values (ph, pw).
+            ph: Padding height.
+            pw: Padding width.
+            Options: 'same' (default), 'valid', or custom tuple.
+        stride (tuple): Stride values (sh, sw) for the convolution operation.
+            sh: Vertical stride.
+            sw: Horizontal stride.
+            Default: (1, 1).
+
+    Returns:
+        numpy.ndarray: Convolved images with shape (m, ch, cw, nc).
+            m: Number of images.
+            ch: Height of the convolved images.
+            cw: Width of the convolved images.
+            nc: Number of kernels/filters.
+    """
+    kh, kw, kc, nc = kernels.shape
+    m, hm, wm, cm = images.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = int(((hm - 1) * sh + kh - hm) / 2) + 1
+        pw = int(((wm - 1) * sw + kw - wm) / 2) + 1
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    ch = int((hm + 2 * ph - kh) / sh) + 1
+    cw = int((wm + 2 * pw - kw) / sw) + 1
+    convoluted = np.zeros((m, ch, cw, nc))
+    for c in range(nc):
+        for h in range(ch):
+            for w in range(cw):
+                square = padded[:, h * sh: h * sh + kh, w * sw: w * sw + kw, :]
+                insert = np.sum(square * kernels[..., c], axis=(1, 2, 3))
+                convoluted[:, h, w, c] = insert
+    return convoluted
