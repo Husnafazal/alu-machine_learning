da614f60c770fcaa5ace0d4decd6f325f51f89e6
husnafazal <h.ahmed1@alustudent.com>
2024-01-19 17:58:02 +0300

    commit

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
+"""Defines a single neuron performing binary classification
+"""
+
 
 import numpy as np
+
 
 class Neuron:
     """
     Define a single neuron performing binary classification.
-
-    Attributes:
-        __W (numpy.ndarray): The weights vector for the neuron.
-        __b (float): The bias for the neuron.
-        __A (float): The activated output of the neuron (prediction).
     """
 
     def __init__(self, nx):
@@ -17,8 +16,7 @@ class Neuron:
         Constructor for neuron class.
 
         Parameters:
-        nx (int): The number of input features to the neuron.
-
+        nx (int): description
         Raises:
         TypeError: If nx is not an integer.
         ValueError: If nx is less than 1.
@@ -46,16 +44,3 @@ class Neuron:
     def A(self):
         """Getter method for __A."""
         return self.__A
-
-# Test script
-if __name__ == "__main__":
-    lib_train = np.load('../data/Binary_Train.npz')
-    X_3D, Y = lib_train['X'], lib_train['Y']
-    X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-    np.random.seed(0)
-    neuron = Neuron(X.shape[0])
-    print(neuron.W)
-    print(neuron.b)
-    print(neuron.A)
-
