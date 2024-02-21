a4dfda5a30ce7e2b0cc9ab8dff24ec7831f1888b
husnafazal <h.ahmed1@alustudent.com>
2024-02-21 18:00:02 +0300

    commit

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-activations, alpha,iterations, save_path="/tmp/model.ckpt"):
-that builds, trains, and saves a neural network classifier
 """
+Defines a function that builds, trains, and saves
+neural network classifier
+"""
+
 
 import tensorflow as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
@@ -12,12 +13,25 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
     """
-    Builds, trains, and saves a neural network classifier.
-    """
+    Builds, trains, and saves a neural network classifier
 
+    parameters:
+        X_train [numpy.ndarray]: contains training input data
+        Y_train [numpy.ndarray]: contains training labels
+        X_valid [numpy.adarray]: contains validation input data
+        Y_valid [numpy.ndarray]: contains validation labels
+        layer_sizes [list]: contains number of nodes in each layer of network
+        activations [list]: contains activation functions for each layer
+        alpha [float]: learning rate
+        iterations [int]: number of iterations to train over
+        save_path [string]: designates path for where to save the model
+
+    returns:
+        path to where model was saved
+    """
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
