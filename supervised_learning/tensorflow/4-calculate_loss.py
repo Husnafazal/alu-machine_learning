e8a2927aff33a92267eae0ff15c57ff95a5f97b7
husnafazal <h.ahmed1@alustudent.com>
2024-02-26 10:14:04 +0300

    commit

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -12,12 +12,12 @@ def compute_loss(y_true, y_estimate):
     y_estimate - tensor representing the model's predictions
     Outputs a tensor with the calculated prediction loss
     """
-    # Conversion of y_estimate to categorical labels was considered but not used
+    # Conversion of y_estimate to categorical labels
     # actual_labels = tf.argmax(y_true, 1)
     # estimated_labels = tf.argmax(y_estimate, 1)
 
     loss = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(
-            labels=y_true, logits=y_estimate), name="softmax_cross_entropy_loss/value")
+            labels=y_true, logits=y_estimate), name="softmax_loss/value")
 
     return loss
