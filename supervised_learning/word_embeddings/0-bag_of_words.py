0da197bf9eeed9ad9f6ffd88ba06c7a5ed1ff8d5
husnafazal <h.ahmed1@alustudent.com>
2024-05-29 13:23:55 +0300

    commit

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+def tf_idf(sentences, vocab=None):
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    embeddings = vectorizer.fit_transform(sentences).toarray()
+    features = vectorizer.get_feature_names_out()
+    return embeddings, features
+
+if __name__ == "__main__":
+    sentences = [
+        "Holberton school is Awesome!",
+        "Machine learning is awesome",
+        "NLP is the future!",
+        "The children are our future",
+        "Our children's children are our grandchildren",
+        "The cake was not very good",
+        "No one said that the cake was not very good",
+        "Life is beautiful"
+    ]
+    vocab = ["awesome", "learning", "children", "cake", "good", "none", "machine"]
+    E, F = tf_idf(sentences, vocab)
+    print(E)
+    print(F)
