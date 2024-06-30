1d31d77c4f0531ed778e747442b45f895974efdc
husnafazal <h.ahmed1@alustudent.com>
2024-06-30 19:59:04 +0200

    commit 6

--- unsupervised_learning/dimensionality_reduction/6-grads.py
+++ unsupervised_learning/dimensionality_reduction/6-grads.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
 """
-This module contain a function that
-calculates gradients of Y
+This module contains a function that
+calculates gradients of Y.
 """
 
 import numpy as np
 Q_affinities = __import__('5-Q_affinities').Q_affinities
 
-
 def grads(Y, P):
     """
     Function that calculates gradients of Y
 
-    Y: numpy.ndarray: (n, ndim)
-        - containing the low dimensional transformation of X
-        n - number of data points
-    P: numpy.ndarray: (n, n)
-        - containing the P affinities of X
+    Parameters:
+    Y (numpy.ndarray): (n, ndim)
+        - Contains the low dimensional transformation of X
+        - n: number of data points
+    P (numpy.ndarray): (n, n)
+        - Contains the P affinities of X
+
     Returns:
-    dY: numpy.ndarray: (n, ndim)
-        - containing the gradients of Y
-    Q: numpy.ndarray: (n, n)
-        - containing the Q affinities of Y
+    dY (numpy.ndarray): (n, ndim)
+        - Contains the gradients of Y
+    Q (numpy.ndarray): (n, n)
+        - Contains the Q affinities of Y
     """
     n, ndim = Y.shape
     Q, num = Q_affinities(Y)
@@ -30,6 +31,6 @@ def grads(Y, P):
     PQ = P - Q
     for i in range(n):
         dY[i, :] = np.sum(
-            np.tile(
-                PQ[:, i] * num[:, i],(ndim, 1)).T * (Y[i,] - Y), 0)
+            np.tile(PQ[:, i] * num[:, i], (ndim, 1)).T * (Y[i, :] - Y), axis=0
+        )
     return dY, Q
