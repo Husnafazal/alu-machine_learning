7abce4c318d54708dcca53540859d6529b8db671
husnafazal <h.ahmed1@alustudent.com>
2024-06-30 19:37:46 +0200

    commit

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,55 +1,31 @@
 #!/usr/bin/env python3
-
 """
-This module contain a function that perfoms PCA
-on a dataset"""
+Defines function that performs principal components analysis (PCA) on dataset
+"""
+
 
 import numpy as np
 
 
 def pca(X, var=0.95):
     """
-    Function that perfoms PCA on a dataset
+    Performs principal components analysis (PCA) on a dataset
 
-    Arguments:
-    X: numpy.ndarray: (n, d) the dataset
-        n - no. of data points
-        d - no. of dimentions in each point
-    var: float: the fraction of variance that the PCA
-    should capture
+    parameters:
+        X [numpy.ndarray of shape (n, d)]: dataset
+            n: number of data points
+            d: number of dimensions in each data point
+            all dimensions have a mean of 0 across all data points
+        var [float]: the fraction of the variance that the PCA
+            transformation should maintain
 
-    Returns:
-    W: numpy.ndarray: (d, k) the matrix that will be used
-    to reduce the dimensionality of the dataset
+    returns:
+        W [numpy.ndarray of shape (d, nd)]: the weights matrix that
+            maintains var fraction of X's original variance
+            d: number of dimensions of each data point
+            nd: new dimensionality of the transformed X
     """
-
-    # Center the data
-    # X = X - np.mean(X, axis=0)
-
-    # # Compute the covariance matrix
-    # cov = np.cov(X, rowvar=False)
-
-    # # Compute the eigenvalues and eigenvectors
-    # eigvals, eigvecs = np.linalg.eig(cov)
-
-    # # Sort the eigenvectors by decreasing eigenvalues
-    # idx = np.argsort(eigvals)[::-1]
-    # eigvecs = eigvecs[:, idx]
-
-    # # Compute the total variance
-    # total_var = np.sum(eigvals)
-
-    # # Compute the number of components to keep
-    # k = 0
-    # for i in range(len(eigvals)):
-    #     if np.sum(eigvals[:i+1]) / total_var >= var:
-    #         k = i + 1
-    #         break
-
-    # # Select the k eigenvectors
-    # W = eigvecs[:, :k]
-
-    # return W
+    # n, d = X.shape
     u, s, v = np.linalg.svd(X)
     ratios = list(x / np.sum(s) for x in s)
     variance = np.cumsum(ratios)
