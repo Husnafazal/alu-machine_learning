d6e4cc35f4c5533cda9357cf86d7386536088862
husnafazal <h.ahmed1@alustudent.com>
2024-07-10 10:20:17 +0200

    shorten

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -4,6 +4,7 @@ Module for Gaussian Process initialization.
 """
 import numpy as np
 
+
 class GaussianProcess:
     """
     Class that represents a noiseless 1D Gaussian process.
@@ -14,12 +15,13 @@ class GaussianProcess:
         Initialize a Gaussian Process.
 
         Args:
-            X_init (numpy.ndarray): Inputs already sampled with the black-box function,
-                                    shape (t, 1).
-            Y_init (numpy.ndarray): Outputs of the black-box function for each input in X_init,
-                                    shape (t, 1).
+            X_init (numpy.ndarray): Inputs already sampled with the black-box
+                                    function, shape (t, 1).
+            Y_init (numpy.ndarray): Outputs of the black-box function for each
+                                    input in X_init, shape (t, 1).
             l (float): Length parameter for the kernel.
-            sigma_f (float): Standard deviation given to the output of the black-box function.
+            sigma_f (float): Standard deviation given to the output of the
+                             black-box function.
         """
         self.X = X_init
         self.Y = Y_init
@@ -29,7 +31,8 @@ class GaussianProcess:
 
     def kernel(self, X1, X2):
         """
-        Compute the covariance kernel matrix between two matrices using the Radial Basis Function (RBF) kernel.
+        Compute the covariance kernel matrix between two matrices using the
+        Radial Basis Function (RBF) kernel.
 
         Args:
             X1 (numpy.ndarray): Input matrix 1, shape (m, 1).
@@ -40,7 +43,6 @@ class GaussianProcess:
         """
         sqdist = (
             np.sum(X1**2, 1).reshape(-1, 1) +
-            np.sum(X2**2, 1) -
-            2 * np.dot(X1, X2.T)
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
         )
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
